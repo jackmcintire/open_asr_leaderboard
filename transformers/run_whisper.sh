58a75d1b8dab10f425f878f8ef5e8275ab93ec8f
“Jack <jack@viennahypertext.com>
2025-08-04 17:59:30 +0000

    resu

--- transformers/run_whisper.sh
+++ transformers/run_whisper.sh
@@ -3,13 +3,11 @@
 export PYTHONPATH="..":$PYTHONPATH
 
 # MODEL_IDs=("openai/whisper-tiny.en" "openai/whisper-small.en" "openai/whisper-base.en" "openai/whisper-medium.en" "openai/whisper-large" "openai/whisper-large-v2" "openai/whisper-large-v3" "distil-whisper/distil-medium.en" "distil-whisper/distil-large-v2" "distil-whisper/distil-large-v3" "nyrahealth/CrisperWhisper")
-MODEL_IDs=("nyrahealth/CrisperWhisper")
-BATCH_SIZE=16
+MODEL_IDs=("jmci/crimson-oath-77")
+BATCH_SIZE=180
 # Leave REVISION unset or empty for no revision
 REVISION=""
 
-# Build revision arg only if REVISION is set and non-empty
-REVISION_ARG=""
 if [ -n "$REVISION" ]; then
     REVISION_ARG="--revision=${REVISION}"
 fi
@@ -20,15 +18,15 @@ for (( i=0; i<${num_models}; i++ ));
 do
     MODEL_ID=${MODEL_IDs[$i]}
 
-    python run_eval.py \
-        --model_id=${MODEL_ID} \
-        --dataset_path="aquavoice/cleaned_dataset_500_turbo" \
-        --dataset="default" \
-        --split="test" \
-        --device=0 \
-        --batch_size=${BATCH_SIZE} \
-        --max_eval_samples=-1 \
-        ${REVISION_ARG}
+    # python run_eval.py \
+    #     --model_id=${MODEL_ID} \
+    #     --dataset_path="aquavoice/cleaned_dataset_500_turbo" \
+    #     --dataset="default" \
+    #     --split="test" \
+    #     --device=0 \
+    #     --batch_size=${BATCH_SIZE} \
+    #     --max_eval_samples=-1 \
+    #     ${REVISION_ARG}
 
     python run_eval.py \
         --model_id=${MODEL_ID} \
