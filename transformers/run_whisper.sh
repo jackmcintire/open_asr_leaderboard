cb3e1a128bc13d270f00c0af4acd142dd90388db
“Jack <jack@viennahypertext.com>
2025-08-06 17:58:26 +0000

    volcanic-meadow

--- transformers/run_whisper.sh
+++ transformers/run_whisper.sh
@@ -3,10 +3,10 @@
 export PYTHONPATH="..":$PYTHONPATH
 
 # MODEL_IDs=("openai/whisper-tiny.en" "openai/whisper-small.en" "openai/whisper-base.en" "openai/whisper-medium.en" "openai/whisper-large" "openai/whisper-large-v2" "openai/whisper-large-v3" "distil-whisper/distil-medium.en" "distil-whisper/distil-large-v2" "distil-whisper/distil-large-v3" "nyrahealth/CrisperWhisper")
-MODEL_IDs=("jmci/volcanic-meadow-91")
+MODEL_IDs=("jmci/sweet-lion-95")
 BATCH_SIZE=180
 # Leave REVISION unset or empty for no revision
-REVISION="e499161c51e822d45a1ab295b685dc9cf27d6850"
+REVISION=""
 
 if [ -n "$REVISION" ]; then
     REVISION_ARG="--revision=${REVISION}"
@@ -17,16 +17,6 @@ num_models=${#MODEL_IDs[@]}
 for (( i=0; i<${num_models}; i++ ));
 do
     MODEL_ID=${MODEL_IDs[$i]}
-
-    python run_eval.py \
-        --model_id=${MODEL_ID} \
-        --dataset_path="aquavoice/cleaned_dataset_full_2x_en_resplit" \
-        --dataset="default" \
-        --split="test" \
-        --device=0 \
-        --batch_size=${BATCH_SIZE} \
-        --max_eval_samples=-1 \
-        ${REVISION_ARG}
 
     python run_eval.py \
         --model_id=${MODEL_ID} \
@@ -102,6 +92,16 @@ do
         --model_id=${MODEL_ID} \
         --dataset_path="hf-audio/esb-datasets-test-only-sorted" \
         --dataset="tedlium" \
+        --split="test" \
+        --device=0 \
+        --batch_size=${BATCH_SIZE} \
+        --max_eval_samples=-1 \
+        ${REVISION_ARG}
+    
+    python run_eval.py \
+        --model_id=${MODEL_ID} \
+        --dataset_path="aquavoice/cleaned_dataset_full_2x_en_resplit" \
+        --dataset="default" \
         --split="test" \
         --device=0 \
         --batch_size=${BATCH_SIZE} \
